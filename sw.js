183df466af67493aa20e9554a373bde856ff4bae
colombovercel-source <colombovercel@gmail.com>
2026-05-07 11:11:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  'https://i.imgur.com/mF88miV.png', // Ikon Naga
+  'https://i.imgur.com/iaQmyzv.png', // Ikon Naga
   'https://i.imgur.com/bfrp75y.png'  // Background (WAJIB dicache biar gak berat)
 ];
 
